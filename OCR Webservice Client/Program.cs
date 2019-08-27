4cb2f0a33479e11df5a1fab846f5ac466d5895d0
San Kanao <sandeep.kanao@gmail.com>
2019-08-27 17:24:44 -0400

    Add files via upload

---
+++ OCR Webservice Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using OCR_Webservice_Model;
+
+namespace OCR_Webservice_Client
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Post("http://localhost:49912/api/tiff", "Tif", "HintDesk.tif");
+			Post("http://localhost:49912/api/pdf", "Pdf", "HintDesk.pdf");
+			Post("http://localhost:49912/api/zip", "Zip", "HintDesk.zip");
+
+			Console.ReadLine();
+		}
+
+		private static void Post(string url, string name, string fileName)
+		{
+			Uri server = new Uri(url);
+			HttpClient httpClient = new HttpClient();
+			httpClient.Timeout = new TimeSpan(httpClient.Timeout.Ticks * 5);
+			StreamContent streamConent = new StreamContent(new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read));
+			MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent();
+			multipartFormDataContent.Add(streamConent, name, fileName);
+
+			HttpResponseMessage responseMessage = httpClient.PostAsync(server, multipartFormDataContent).Result;
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				IList<HDFile> hdFiles = responseMessage.Content.ReadAsAsync<IList<HDFile>>().Result;
+
+				foreach (var item in hdFiles)
+				{
+					Console.WriteLine(item.Text);
+				}
+			}
+		}
+	}
+}
---
+++ OCR Webservice Model/HDFile.cs
@@ -0,0 +1,18 @@
+﻿namespace OCR_Webservice_Model
+{
+	public class HDFile
+	{
+		public HDFile(string name, string text, string tag = null)
+		{
+			Name = name;
+			Text = text;
+			Tag = tag;
+		}
+
+		public string Name { get; set; }
+
+		public string Tag { get; set; }
+
+		public string Text { get; set; }
+	}
+}
